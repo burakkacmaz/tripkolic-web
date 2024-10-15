1812031572ceeff8b5836f9ee3b58e3f3c8cf067
e <67810792+burakkacmaz@users.noreply.github.com>
2024-10-16 00:59:32 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzd-z_FWY8aF9QuYTI63-lOk05BQ16mfA",
-  authDomain: "tripkolic-fa7f1.firebaseapp.com",
-  projectId: "tripkolic-fa7f1",
-  storageBucket: "tripkolic-fa7f1.appspot.com",
-  messagingSenderId: "875542314608",
-  appId: "1:875542314608:web:22152acc03690d69dcbeb5",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
